14021e39b2c7711baaa324560935087c35be8954
Juan S <juanandrees@hotmail.com>
2020-10-19 23:14:24 -0500

    Solution solved problems fixed

--- src/homework/fibonacci/main.py
+++ src/homework/fibonacci/main.py
@@ -1,21 +1,35 @@
 from typing import List
+import fire
 
 from fintools.settings import get_logger
+from fintools.utils import timeit, method_caching
 
 logger = get_logger(name=__name__)
 
 
 class Main:
-
     def __init__(self):
         logger.info("Main object initialized.")
 
-    def element(self, i: int,position: int) -> int:
-        return 1 if (position <= 1 or position => i) else /
-            self.element(i , position = position-2) + self.element(i )
+    def fib_rec(self, n):
+        if n == 1:
+            return [0]
+        elif n == 2:
+            return [0, 1]
+        else:
+            x = self.fib_rec(n - 1)
+            x.append(sum(x[:-3:-1]))
+            return x
 
+    @method_caching
+    def element(self, position: int) -> int:
+        if isinstance(position, int):
+            return position
 
-
-
+    @timeit(logger=logger)
     def sequence(self, length: int) -> List[int]:
-        pass
+        return self.fib_rec(length)
+
+
+if __name__ == "__main__":
+    fire.Fire(Main)
