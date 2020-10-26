19d547d70e6211a3fec72bb6be3c9d95f5f7ee37
Juan S <juanandrees@hotmail.com>
2020-10-25 20:07:24 -0600

    Files for homework SIC-INDUSTRY, had to modify utils.py as well

--- src/homework/sic-industry/main.py
+++ src/homework/sic-industry/main.py
@@ -1,9 +1,7 @@
-import os
 import json
-from typing import Dict
 
 from fintools.settings import get_logger
-from fintools.utils import StringWrapper, timeit
+from fintools.utils import StringWrapper, timeit, print_terminal
 
 from .settings import (
     INDUSTRY_SEARCH_DEFAULT_FILENAME,
@@ -16,6 +14,28 @@ logger = get_logger(name=__name__)
 class Main:
     threshold = INDUSTRY_SEARCH_DEFAULT_THRESHOLD
 
+    def _recursive_search(self, node, string_wrapper, exact):
+        title = node["title"]
+        children = node["children"]
+        new_children = []
+        for child in children:
+            is_child_valid = self._recursive_search(child, string_wrapper, exact)
+            if is_child_valid:
+                new_children.append(child)
+        node["children"] = new_children
+        search = len(new_children) or string_wrapper.boolean_search(title, exact=exact,
+                                                                    threshold=0.5, reverse=True)
+        return search
+
     @timeit(logger=logger)
-    def search(self, title: str, exact: bool = False, file: str = INDUSTRY_SEARCH_DEFAULT_FILENAME) -> str:
-        pass
+    @print_terminal(logger)
+    def search(self, title: str, exact: bool = False, file: str = INDUSTRY_SEARCH_DEFAULT_FILENAME) -> list:
+        with open(file, 'r') as f:
+            jason = json.load(f)
+            target_title = StringWrapper(value=title)
+            children = jason["children"]
+            new_children = []
+            for child in children:
+                if self._recursive_search(child, target_title, exact=exact):
+                    new_children.append(child)
+            return new_children
