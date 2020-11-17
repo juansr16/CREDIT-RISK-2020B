60972645e876bd68dfd63f05e758251b8a376a28
Juan S <juanandrees@hotmail.com>
2020-11-17 10:16:29 -0600

    Solution for amortization HW

--- fintools/src/fintools/amortization.py
+++ fintools/src/fintools/amortization.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 
@@ -14,10 +13,35 @@ class Amortization:
 
     @property
     def annuity(self) -> float:
-        raise NotImplementedError
+        return self.rate * self.amount/(1-(1+self.rate)**(-self.n))
 
     def get_table(self, save_file: Optional[str] = None) -> pd.DataFrame:
-        raise NotImplementedError
+        rows = []
+        amount = self.amount
+        a = 'nan'
+        principal = 'nan'
+        rate_value = 'nan'
+        for i in range(self.n+1):
+            rows.append(
+                {"B": amount, "A": a, "P": principal, "I": rate_value})
+            table = pd.DataFrame(rows).rename_axis("t").reset_index()
+            a = self.annuity
+            rate_value = amount * self.rate
+            principal = a - rate_value
+            amount = amount - principal
+            if save_file:
+                table.savefig(save_file)
+        return pd.DataFrame(rows).rename_axis("t").reset_index()
 
-    def plot(self, show: bool = False, save_file: Optional[str] = None) -> None:
-        raise NotImplementedError
+    def plot(self, show: bool = False, save_file: Optional[str] = None):
+        df = self.get_table()
+        df = df.astype('float')
+        plot = df.plot.bar(x="t", y=["P", "I"], stacked=True)
+        fig = plot.get_figure()
+        plt.grid()
+        plt.show()
+        if show:
+            return fig
+        if save_file:
+            fig.savefig(save_file)
+        return fig
--- src/homework/amortization/main.py
+++ src/homework/amortization/main.py
@@ -7,12 +7,21 @@ class Main:
 
     @staticmethod
     def annuity(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        amortization = Amortization(amount, rate, n)
+        return amortization.annuity
 
     @staticmethod
     def table(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        amortization = Amortization(amount, rate, n)
+        table = amortization.get_table()
+        print(table)
+        if save_file:
+            table.to_csv(save_file)
 
     @staticmethod
     def plot(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        amortization = Amortization(amount, rate, n)
+        figure = amortization.plot()
+        if save_file:
+            figure.savefig(save_file)
+        return figure
